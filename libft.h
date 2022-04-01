9cf82722371cfa8aa3e088aa86da8e11a6176503
aibikand <102603300+aibikand@users.noreply.github.com>
2022-04-01 16:42:10 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	LIBFT_H
+#define LIBFT_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <xlocale.h>
 
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c, locale_t loc);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+#endif
